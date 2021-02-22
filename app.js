ad0db43b87620aa07eff1cba830760a95593dfc3
Amey Jakate <ameyjakate2001@gmail.com>
2021-02-21 22:25:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const routes = require('./Routes/API-Routes');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true,
 }))
